8fc227273be5dfb17fe4bfb0075e3a1a224a424b
Barto Alheiros <alheirosb@gmail.com>
2024-02-05 17:04:49 -0300

    models

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    
+    STATUS = (
+        (1,'Doing'),
+        (2, 'Done'),
+    )
+    
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.CharField(
+        max_length=1,
+        choices=STATUS,
+    )
+    
